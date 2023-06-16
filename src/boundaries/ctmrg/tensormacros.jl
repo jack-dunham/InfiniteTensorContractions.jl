9c7625d4939f1cc2feb2d7cdb48f8eadaad5f218
Jack Dunham <ucapunh@ucl.ac.uk>
2023-06-16 11:27:36 +0100

    bugfixes, more peps support

--- src/boundaries/ctmrg/tensormacros.jl
+++ src/boundaries/ctmrg/tensormacros.jl
@@ -31,12 +31,10 @@ end
 ## SINGLE LAYER
 
 function _projectedge!(
-    t_dst::T,
-    t_src::T, 
-    m:M, 
-    u::UV, 
-    v::UV
-) where {S,T<:AbstractTensorMap{S,1,2}, M<:AbstractTensorMap{0,4}, UV<:AbstractTensorMap{2,1}}
+    t_dst::T, t_src::T, m::M, u::UV, v::UV
+) where {
+    S,T<:AbstractTensorMap{S,1,2},M<:AbstractTensorMap{S,0,4},UV<:AbstractTensorMap{S,2,1}
+}
     @tensoropt t_dst[h0; v0_d v0_u] =
         t_src[h1; v3 v1] * m[h0 v4 h1 v2] * u[v1 v2; v0_u] * v[v3 v4; v0_d]
     return t_dst
@@ -45,23 +43,18 @@ end
 ## DOUBLE LAYER
 
 function _projectedge!(
-    t_dst::T,
-    t_src::T,
-    ma::M,
-    u::UV, 
-    v::UV,
-)where {S,N,T<:AbstractTensorMap{S,2,2}, M<:AbstractTensorMap{N,4}, UV<:AbstractTensorMap{3,1}}
+    t_dst::T, t_src::T, ma::M, u::UV, v::UV
+) where {
+    S,N,T<:AbstractTensorMap{S,2,2},M<:AbstractTensorMap{S,N,4},UV<:AbstractTensorMap{S,3,1}
+}
     return _projectedge!(t_dst, t_src, ma, ma, u, v)
 end
 
 function _projectedge!(
-    t_dst::T,
-    t_src::T,
-    ma::M,
-    mb::M,
-    u::UV, 
-    v::UV,
-) where {S,T<:AbstractTensorMap{S,2,2}, M<:AbstractTensorMap{1,4}, UV<:AbstractTensorMap{3,1}}
+    t_dst::T, t_src::T, ma::M, mb::M, u::UV, v::UV
+) where {
+    S,T<:AbstractTensorMap{S,2,2},M<:AbstractTensorMap{S,1,4},UV<:AbstractTensorMap{S,3,1}
+}
     @tensoropt (
         k => 2,
         D1 => D,
@@ -78,8 +71,8 @@ function _projectedge!(
         x4 => D,
     ) t_dst[D1 D5; x4 x2] =
         t_src[D3 D7; x3 x1] *
-        ma[k ; D1 D2 D3 D4] *
-        (mb')[D5 D6 D7 D8; k ] *
+        ma[k; D1 D2 D3 D4] *
+        (mb')[D5 D6 D7 D8; k] *
         u[x1 D4 D8; x2] *
         v[x3 D2 D6; x4]
 
@@ -87,13 +80,10 @@ function _projectedge!(
 end
 
 function _projectedge!(
-    t_dst::T,
-    t_src::T,
-    ma::M,
-    mb::M,
-    u::UV, 
-    v::UV,
-) where {S,T<:AbstractTensorMap{S,2,2}, M<:AbstractTensorMap{2,4}, UV<:AbstractTensorMap{3,1}}
+    t_dst::T, t_src::T, ma::M, mb::M, u::UV, v::UV
+) where {
+    S,T<:AbstractTensorMap{S,2,2},M<:AbstractTensorMap{S,2,4},UV<:AbstractTensorMap{S,3,1}
+}
     @tensoropt (
         k => 2,
         b => 2,
@@ -123,15 +113,10 @@ end
 
 # O(χ^2 D^8) or D^10
 function halfcontract(
-    C1_00::C, 
-    T1_10::T,
-    T1_20::T, 
-    C2_30::C, 
-    T4_01::T, 
-    M_11::M, 
-    M_21::M, 
-    T2_31::T
-) where {S, C<:AbstractTensorMap{S,1,1},T<:AbstractTensorMap{S,1,2}, M<:AbstractTensorMap{S,0,4}}
+    C1_00::C, T1_10::T, T1_20::T, C2_30::C, T4_01::T, M_11::M, M_21::M, T2_31::T
+) where {
+    S,C<:AbstractTensorMap{S,1,1},T<:AbstractTensorMap{S,1,2},M<:AbstractTensorMap{S,0,4}
+}
     @tensoropt out[v8 v7; v5 v6] :=
         C1_00[h1 v1] *
         T1_10[v2; h2 h1] *
@@ -148,16 +133,13 @@ end
 ## DOUBLE LAYER METHODS
 
 function halfcontract(
-    C1_00::C,
-    T1_10::T,
-    T1_20::T,
-    C2_30::C,
-    T4_01::T,
-    M_11a::M
-    M_21a::M,
-    T2_31::T,
-) where {S, N, C<:AbstractTensorMap{S,1,1}, T<:AbstractTensorMap{S,2,2}, M<:AbstractTensorMap{S,N,4}}
-    return halfcontract(C1_00, T1_10,T1_20,C2_30,T4_01,M_11a,M_11a, M_21a, M_21a, T2_31)
+    C1_00::C, T1_10::T, T1_20::T, C2_30::C, T4_01::T, M_11a::M, M_21a::M, T2_31::T
+) where {
+    S,N,C<:AbstractTensorMap{S,1,1},T<:AbstractTensorMap{S,2,2},M<:AbstractTensorMap{S,N,4}
+}
+    return halfcontract(
+        C1_00, T1_10, T1_20, C2_30, T4_01, M_11a, M_11a, M_21a, M_21a, T2_31
+    )
 end
 
 function halfcontract(
@@ -166,12 +148,14 @@ function halfcontract(
     T1_20::T,
     C2_30::C,
     T4_01::T,
-    M_11a::M
+    M_11a::M,
     M_11b::M,
     M_21a::M,
     M_21b::M,
     T2_31::T,
-) where {S, C<:AbstractTensorMap{S,1,1}, T<:AbstractTensorMap{S,2,2}, M<:AbsTen{1,4}}
+) where {
+    S,C<:AbstractTensorMap{S,1,1},T<:AbstractTensorMap{S,2,2},M<:AbstractTensorMap{S,1,4}
+}
     @tensoropt (
         k1 => 2,
         k2 => 2,
@@ -197,10 +181,10 @@ function halfcontract(
         x6 => D,
         x7 => D,
     ) top[x7 D6 E6; x6 D2 E2] :=
-        M_11a[k1 ; D1 D2 D3 D4] *
-        M_21a[k2 ; D5 D6 D1 D8] *
-        (M_11b)'[E1 E2 E3 E4; k1 ] *
-        (M_21b)'[E5 E6 E1 E8; k2 ] *
+        M_11a[k1; D1 D2 D3 D4] *
+        M_21a[k2; D5 D6 D1 D8] *
+        (M_11b)'[E1 E2 E3 E4; k1] *
+        (M_21b)'[E5 E6 E1 E8; k2] *
         C1_00[x1 x4] *
         T1_10[D4 E4; x2 x1] *
         T1_20[D8 E8; x3 x2] *
@@ -217,12 +201,14 @@ function halfcontract(
     T1_20::T,
     C2_30::C,
     T4_01::T,
-    M_11a::M
+    M_11a::M,
     M_11b::M,
     M_21a::M,
     M_21b::M,
     T2_31::T,
-) where {S, C<:AbstractTensorMap{S,1,1}, T<:AbstractTensorMap{S,2,2}, M<:AbsTen{2,4}}
+) where {
+    S,C<:AbstractTensorMap{S,1,1},T<:AbstractTensorMap{S,2,2},M<:AbstractTensorMap{S,2,4}
+}
     @tensoropt (
         k1 => 2,
         k2 => 2,
--- src/boundaries/vumps/tensormacros.jl
+++ src/boundaries/vumps/tensormacros.jl
@@ -1,14 +1,63 @@
 # mpo-like
 function applyhac!(
-    hac::AbsTen{1,2}, ac::AbsTen{1,2}, fl::AbsTen{1,2}, fr::AbsTen{1,2}, m::AbsTen{0,4}
-)
+    hac::AbstractTensorMap{S,1,2},
+    ac::AbstractTensorMap{S,1,2},
+    fl::AbstractTensorMap{S,1,2},
+    fr::AbstractTensorMap{S,1,2},
+    m::AbstractTensorMap{S,0,4},
+) where {S}
     @tensoropt hac[D2; x4 x3] =
         ac[D4; x2 x1] * fl[D3; x1 x3] * m[D1 D2 D3 D4] * fr[D1; x2 x4]
 end
+# peps-like
+function applyhac!(
+    hac::AbstractTensorMap{S,2,2},
+    ac::AbstractTensorMap{S,2,2},
+    fl::AbstractTensorMap{S,2,2},
+    fr::AbstractTensorMap{S,2,2},
+    ma::AbstractTensorMap{S,N,4},
+) where {S,N}
+    return applyhac!(hac, ac, fl, fr, ma, ma)
+end
+
+function applyhac!(
+    hac::AbstractTensorMap{S,2,2},
+    ac::AbstractTensorMap{S,2,2},
+    fl::AbstractTensorMap{S,2,2},
+    fr::AbstractTensorMap{S,2,2},
+    ma::AbstractTensorMap{S,1,4},
+    mb::AbstractTensorMap{S,1,4},
+) where {S}
+    @tensoropt (
+        k => 2,
+        D1 => D,
+        E1 => D,
+        D2 => D,
+        E2 => D,
+        D3 => D,
+        E3 => D,
+        D4 => D,
+        E4 => D,
+        x1 => D,
+        x2 => D,
+        x3 => D,
+        x4 => D,
+    ) hac[D2 E2; x4 x3] =
+        ac[D4 E4; x2 x1] *
+        fl[D3 E3; x1 x3] *
+        ma[k; D1 D2 D3 D4] *
+        (mb')[E1 E2 E3 E4; k] *
+        fr[D1 E1; x2 x4]
+end
 # pepo-like
 function applyhac!(
-    hac::AbsTen{2,2}, ac::AbsTen{2,2}, fl::AbsTen{2,2}, fr::AbsTen{2,2}, m::AbsTen{2,4}
-)
+    hac::AbstractTensorMap{S,2,2},
+    ac::AbstractTensorMap{S,2,2},
+    fl::AbstractTensorMap{S,2,2},
+    fr::AbstractTensorMap{S,2,2},
+    ma::AbstractTensorMap{S,2,4},
+    mb::AbstractTensorMap{S,2,4},
+) where {S}
     @tensoropt (
         k => 2,
         b => 2,
@@ -27,16 +76,26 @@ function applyhac!(
     ) hac[D2 E2; x4 x3] =
         ac[D4 E4; x2 x1] *
         fl[D3 E3; x1 x3] *
-        m[k b; D1 D2 D3 D4] *
-        (m')[E1 E2 E3 E4; k b] *
+        ma[k b; D1 D2 D3 D4] *
+        (mb')[E1 E2 E3 E4; k b] *
         fr[D1 E1; x2 x4]
 end
 
 # mpo-like
-function applyhc!(hc::AbsTen{0,2}, c::AbsTen{0,2}, fl::AbsTen{1,2}, fr::AbsTen{1,2})
+function applyhc!(
+    hc::AbstractTensorMap{S,0,2},
+    c::AbstractTensorMap{S,0,2},
+    fl::AbstractTensorMap{S,1,2},
+    fr::AbstractTensorMap{S,1,2},
+) where {S}
     @tensoropt hc[x4 x3] = c[x2 x1] * fl[D; x1 x3] * fr[D; x2 x4]
 end
-# pepo-like
-function applyhc!(hc::AbsTen{0,2}, c::AbsTen{0,2}, fl::AbsTen{2,2}, fr::AbsTen{2,2})
+# pepx-like
+function applyhc!(
+    hc::AbstractTensorMap{S,0,2},
+    c::AbstractTensorMap{S,0,2},
+    fl::AbstractTensorMap{S,2,2},
+    fr::AbstractTensorMap{S,2,2},
+) where {S}
     @tensoropt hc[x4 x3] = c[x2 x1] * fl[D E; x1 x3] * fr[D E; x2 x4]
 end
